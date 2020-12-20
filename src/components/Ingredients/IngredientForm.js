2c135f858c460e353d471a0c391a10e5861dbf7f
Paweł Grajewski <unionproject88@gmail.com>
2020-12-20 23:09:41 +0100

    add event update state

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -15,17 +15,18 @@ const IngredientForm = React.memo((props) => {
       <Card>
         <form onSubmit={submitHandler}>
           <div className="form-control">
-            <label htmlFor="title">Name #edited</label>
+            <label htmlFor="title">Name</label>
             <input
               type="text"
               id="title"
               value={inputState[0].title}
-              onChange={(event) =>
-                inputState[1]({
-                  title: event.target.value,
-                  amount: inputState[0].amount
-                })
-              }
+              onChange={(event) => {
+                const newTitle = event.target.value;
+                inputState[1]((prevInputState) => ({
+                  title: newTitle,
+                  amount: prevInputState.amount
+                }));
+              }}
             />
           </div>
           <div className="form-control">
@@ -34,12 +35,13 @@ const IngredientForm = React.memo((props) => {
               type="number"
               id="amount"
               value={inputState[0].amount}
-              onChange={(event) =>
-                inputState[1]({
-                  amount: event.target.value,
-                  title: inputState[0].title
-                })
-              }
+              onChange={(event) => {
+                const newAmount = event.target.value;
+                inputState[1]((prevInputState) => ({
+                  amount: newAmount,
+                  title: prevInputState.title
+                }));
+              }}
             />
           </div>
           <div className="ingredient-form__actions">
